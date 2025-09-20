93da557e47a6d85a02311590c3d29602b4dc2a73
aman-webdev <aman.nabi02@gmail.com>
2025-09-21 00:33:31 +0530

    use webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    '@langchain/community',
+    '@langchain/core',
+    '@langchain/openai',
+    '@langchain/qdrant',
+    '@langchain/textsplitters',
+    'langchain',
+    'pdf-parse',
+    'jsdom',
+    'html-to-text',
+    '@prisma/client'
+  ],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ...config.externals,
+        'canvas',
+        'sharp',
+        'onnxruntime-node'
+      ];
+    }
+    return config;
+  },
+  output: 'standalone'
 };
 
 export default nextConfig;
